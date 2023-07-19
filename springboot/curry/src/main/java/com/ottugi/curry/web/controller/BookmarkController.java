6b8f0fe294156bf35757a1b39924d50339222258
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-07-19 16:14:45 +0900

    Fix Bookmark search

--- springboot/curry/src/main/java/com/ottugi/curry/web/controller/BookmarkController.java
+++ springboot/curry/src/main/java/com/ottugi/curry/web/controller/BookmarkController.java
@@ -37,29 +37,18 @@ public class BookmarkController {
         return ResponseEntity.ok().body(bookmarkService.getBookmarkAll(userId, page, size));
     }
 
-    @GetMapping("/api/bookmark/searchByName")
-    @ApiOperation(value = "북마크 레시피 리스트 중 이름으로 검색", notes = "북마크 레시피 리스트에서 이름으로 검색하여 리턴합니다.")
+    @GetMapping("/api/bookmark/searchBookmark")
+    @ApiOperation(value = "북마크 레시피 리스트 중 이름과 옵션으로 검색", notes = "북마크 레시피 리스트에서 이름과 옵션으로 검색하여 리턴합니다.")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "userId", value = "회원 기본키", example = "1", required = true),
             @ApiImplicitParam(name = "page", value = "페이지 번호", example = "1"),
             @ApiImplicitParam(name = "size", value = "페이지 사이즈", example = "10"),
-            @ApiImplicitParam(name = "name", value = "레시피 이름", example = "고구마", required = true)
-    })
-    public ResponseEntity<Page<BookmarkListResponseDto>> searchByName(@RequestParam Long userId, int page, int size, String name) {
-        return ResponseEntity.ok().body(bookmarkService.searchByName(userId, page, size, name));
-    }
-
-    @GetMapping("/api/bookmark/searchByOption")
-    @ApiOperation(value = "북마크 레시피 리스트 중 옵션으로 검색", notes = "북마크 레시피 리스트에서 옵션(시간/난이도/구성)으로 검색하여 리턴합니다.")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId", value = "회원 기본키", example = "1", required = true),
-            @ApiImplicitParam(name = "page", value = "페이지 번호", example = "1"),
-            @ApiImplicitParam(name = "size", value = "페이지 사이즈", example = "10"),
+            @ApiImplicitParam(name = "name", value = "레시피 이름", example = "고구마"),
             @ApiImplicitParam(name = "time", value = "레시피 시간", example = "15분 이내"),
             @ApiImplicitParam(name = "difficulty", value = "레시피 난이도", example = "아무나"),
             @ApiImplicitParam(name = "composition", value = "레시피 구성", example = "든든하게")
     })
-    public ResponseEntity<Page<BookmarkListResponseDto>> searchByOption(@RequestParam Long userId, int page, int size, String time, String difficulty, String composition) {
-        return ResponseEntity.ok().body(bookmarkService.searchByOption(userId, page, size, time, difficulty, composition));
+    public ResponseEntity<Page<BookmarkListResponseDto>> searchBookmark(@RequestParam Long userId, int page, int size, String name, String time, String difficulty, String composition) {
+        return ResponseEntity.ok().body(bookmarkService.searchBookmark(userId, page, size, name, time, difficulty, composition));
     }
 }
