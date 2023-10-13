1fe85b8857c0efc26b56a9b1cad4a4fdbd3ec30c
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-10-13 12:06:44 +0900

    Add getArCharacterType method for ar

--- springboot/curry/src/main/java/com/ottugi/curry/web/dto/recipe/RecipeListResponseDto.java
+++ springboot/curry/src/main/java/com/ottugi/curry/web/dto/recipe/RecipeListResponseDto.java
@@ -31,6 +31,9 @@ public class RecipeListResponseDto {
     @ApiModelProperty(notes = "북마크 유무", example = "true")
     private Boolean isBookmark;
 
+    @ApiModelProperty(notes = "재료 메인 장르", example = "vegetable")
+    private String mainGenre;
+
     public RecipeListResponseDto(Recipe recipe, Boolean isBookmark) {
         this.recipeId = recipe.getRecipeId();
         this.name = recipe.getName();
@@ -40,5 +43,65 @@ public class RecipeListResponseDto {
         this.composition = recipe.getComposition().getComposition();
         this.ingredients = recipe.getIngredients();
         this.isBookmark = isBookmark;
+        this.mainGenre = getArCharacterType(recipe);
+    }
+
+    // 추천 레시피에 따른 3D 모델 캐릭터 조회
+    private String getArCharacterType(Recipe recipe) {
+        String[] parts = recipe.getGenre().split("\\|");
+        if (parts.length > 0) {
+            String mainGenre = parts[0];
+            String character = "";
+            System.out.println(mainGenre);
+            switch (mainGenre) {
+                case "ing1":
+                case "ing2":
+                case "ing3":
+                case "ing4":
+                    character = "meat";
+                    break;
+                case "ing5":
+                case "ing6":
+                case "ing7":
+                case "ing8":
+                    character = "fish";
+                    break;
+                case "ing9":
+                    character = "kimchi";
+                    break;
+                case "ing10":
+                    character = "tofu";
+                    break;
+                case "ing11":
+                    character = "egg";
+                    break;
+                case "ing12":
+                    character = "mushroom";
+                    break;
+                case "ing13":
+                case "ing14":
+                case "ing15":
+                case "ing16":
+                case "ing17":
+                case "ing18":
+                    character = "vegetable";
+                    break;
+                case "ing19":
+                case "ing20":
+                case "ing21":
+                case "ing22":
+                case "ing23":
+                case "ing24":
+                    character = "fruit";
+                    break;
+                case "ing25":
+                    character = "milk";
+                    break;
+            }
+            return character;
+        }
+        else {
+            return null;
+        }
     }
 }
